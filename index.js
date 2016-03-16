bbec4eae853c32a668784bf817230948f2c1d418
F T <f_t@hotmail.ca>
2016-03-15 22:16:05 -0700

    Some progress. Found a way to calculate relative position of View.

--- index.js
+++ index.js
@@ -17,21 +17,11 @@ class ZindexWrapper extends Component {
   }
   getChildContext() {
     return {
-      addToZindex: (zIndex, component, nodeHandle) => {
+      addToZindex: (zIndex, component, nodeRef) => {
         const wrapperRef = React.findNodeHandle(this.refs.wrapper);
-        UIManager.measureLayout(nodeHandle, wrapperRef,this.logMeasure, this.logMeasure);
-        const {layers} = this.state;
-        if (zIndex <= 0 || zIndex > 10) {
-          // TODO(ftufek): document this limitation and investigate
-          // if there's a useful usecase for having more than 10 layers
-          return;
-        }
-        if (layers[zIndex]) {
-          layers[zIndex].push(component);
-        } else {
-          layers[zIndex] = [component];
-        }
-        this.setState({layers});
+        nodeRef.measureLayout(wrapperRef,
+               (x, y, width, height)=>{this.addToLayer(zIndex, component, x, y, width, height);},
+               ()=>{console.log(arguments);});
       },
       removeFromZindex: function(component) {
 
@@ -39,13 +29,26 @@ class ZindexWrapper extends Component {
     };
   }
 
-  logMeasure(ox, oy, width, height, px, py) {
-    console.log("ox: " + ox);
-    console.log("oy: " + oy);
-    console.log("width: " + width);
-    console.log("height: " + height);
-    console.log("px: " + px);
-    console.log("py: " + py);
+  addToLayer(zIndex, component, x, y, width, height) {
+    const {layers} = this.state;
+    if (zIndex <= 0 || zIndex > 10) {
+      // TODO(ftufek): document this limitation and investigate
+      // if there's a useful usecase for having more than 10 layers
+      return;
+    }
+    const obj = {
+      component,
+      x,
+      y,
+      width,
+      height,
+    };
+    if (layers[zIndex]) {
+      layers[zIndex].push(obj);
+    } else {
+      layers[zIndex] = [obj];
+    }
+    this.setState({layers});
   }
 
   render() {
@@ -55,8 +58,21 @@ class ZindexWrapper extends Component {
       // TODO(ftufek): make this more efficient...
       const layer = layers[i];
       if (layer) {
-        for (let j = 0; j <= layer.length; j++) {
-          layerComponents.push(layer[j]);
+        for (let j = 0; j < layer.length; j++) {
+          //layerComponents.push(layer[j]);
+          const cLayer = layer[j];
+          const styleobj = {
+            'position': 'absolute',
+            'top': cLayer.y,
+            'left': cLayer.x,
+            'width': cLayer.width,
+            'height': cLayer.height,
+          };
+          layerComponents.push(
+            <View style={styleobj}>
+              {cLayer.component}
+            </View>
+          );
         }
       }
     }
@@ -74,11 +90,13 @@ class ZindexWrapper extends Component {
   };
 }
 
-class Zindex extends Component {
+class ZindexView extends Component {
   componentDidMount() {
-    this.context.addToZindex(1, this.props.children,React.findNodeHandle(this.refs.vref));
-    UIManager.measure(React.findNodeHandle(this.refs.vref), this.logMeasure);
+    //this.context.addToZindex(1, this.props.children,React.findNodeHandle(this.refs.vref));
+    //UIManager.measure(React.findNodeHandle(this.refs.vref), this.logMeasure);
     //this.props.children.measure(this.logMeasure);
+    //debugger;
+    //this.refs.vref.measure([>React.findNodeHandle(this.refs.vref),<]this.logMeasure);
   }
 
   logMeasure(ox, oy, width, height, px, py) {
@@ -90,9 +108,18 @@ class Zindex extends Component {
     console.log("py: " + py);
   }
 
+  updateLayoutMeasurements() {
+    //this.refs.vref.measure(this.logMeasure);
+    this.context.addToZindex(1, this.props.children,this.refs.vref);
+  }
+
   render() {
     return (
-      <View ref="vref" style={styles.wrapper}>
+      <View style={styles.zindex} ref="vref" onLayout={
+        (event) => {
+          this.updateLayoutMeasurements();
+        }
+      }>
       </View>
     );
   }
@@ -116,9 +143,17 @@ const styles = StyleSheet.create({
     right: 0,
     backgroundColor: 'rgba(255, 0, 0, 0.2)',
   },
+  zindex: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'black',
+  },
 });
 
 module.exports = {
   ZindexWrapper,
-  Zindex,
+  ZindexView,
 };
